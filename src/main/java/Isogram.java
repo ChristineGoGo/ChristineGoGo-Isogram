10886486ffcad775fd30b9118ca96a2c5b03d3b9
Christine Gogo <brandywang22@gmail.com>
2025-01-29 17:01:05 +0000

    Is isogram challenge

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,5 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class Isogram {
+    /**
+     * create a Map of all the alphabet letters with a count of 0
+     * @return Map of all the lowercase alphabet characters with count of each letter being 0
+     */
+    public Map<String, Integer> getAlphabet() {
+        Map<String, Integer> alphabets = new HashMap<>();
+        char currentChar = 97;
+        String currentString = "";
+
+        for (int i = 0; i < 26; i++) {
+            currentString += currentChar;
+            alphabets.put(currentString,0);
+            currentChar += 1;
+            currentString = "";
+        }
+        return alphabets;
+    }
     /**
      * An Isogram is a word where no letters are repeated. You may assume that there will only be lowercase
      * letters in the String.
@@ -13,6 +33,26 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        // return false;
+        Map<String, Integer> alphabetFrequency = new HashMap<>(getAlphabet());
+        char currentChar;
+        int currentCount = 0;
+        String currentString = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            currentChar = str.charAt(i);
+            currentString += currentChar;
+            currentCount = alphabetFrequency.get(currentString);
+            currentCount += 1;
+            alphabetFrequency.put(currentString, currentCount);
+
+            if (currentCount > 1) {
+                return false;
+            }
+            currentString = "";
+
+        }
+        return true;
+
     }
 }
